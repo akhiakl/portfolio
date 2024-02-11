1ebe26f27ac8feb4ec7146d995bef5f7844d12bd
Akhil <akhilthedevz@gmail.com>
2024-02-12 01:32:45 +0530

    speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import './styles/globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './styles/icomoon.css'
 import './styles/preloader.css'
-import {LoadingScreen} from '@/components'
+import { LoadingScreen } from '@/components'
 import 'aos/dist/aos.css'
 
 export const metadata: Metadata = {
@@ -17,14 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html>
-        <head>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-        </head>
-        <body className="overflow-hidden">
-          <LoadingScreen />
-          {children}
-        </body>
-      </html>
+      <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      </head>
+      <body className="overflow-hidden">
+        <LoadingScreen />
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
   )
 }
